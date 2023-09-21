9490bf2ec03d8e76dc1e3b76419336c78940a284
takahito1011 <144980795+takahito1011@users.noreply.github.com>
2023-09-21 04:34:19 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
     results =["大吉","中吉","小吉","吉","強","大凶"]
     result = random.choice(results)
- st.write(f"結果:{result}")
+    st.write(f"結果:{result}")
