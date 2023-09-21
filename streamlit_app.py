e911937c507ffc6a031c7199413ea291b95b5411
takahito1011 <144980795+takahito1011@users.noreply.github.com>
2023-09-21 23:52:11 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit as st
 import random
-st.title("おみくじアプリ")
-if st.button("おみくじを引く"):
-    results =["大吉","中吉","小吉","吉","強","大凶"]
-    result = random.choice(results)
-    st.write(f"結果:{result}")
+
+# ゲームの開始
+st.title("数字当てゲーム")
+
+# ランダムな数字を生成
+number = random.randint(1, 100)
+
+# ユーザーに数字を入力させる
+guess = st.number_input("数字を入力してください", min_value=1, max_value=100)
+
+# ユーザーの入力とランダムな数字を比較する
+if guess == number:
+    st.write("正解です！")
+else:
+    st.write("不正解です。正解は{}でした。".format(number))
